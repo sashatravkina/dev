b1944fb5599f3f22b14c5fb38ca36bd62732e463
Alexandra Travkina <hello@fixel.ru>
2021-06-07 01:51:37 +0700

    Upload

--- views/directory/directory/members.php
+++ views/directory/directory/members.php
@@ -13,7 +13,7 @@ use humhub\modules\directory\widgets\UserTagList;
 use humhub\modules\user\widgets\Image;
 
 ?>
-<div class="panel panel-default section-member">
+<div class="panel panel-default section-members">
 
     <div class="panel-heading">
         <?php if ($group === null) : ?>
@@ -25,17 +25,11 @@ use humhub\modules\user\widgets\Image;
 
     <div class="panel-body">
         <?= Html::beginForm('', 'get', ['class' => 'form-search']); ?>
-        <div class="row">
-            <div class="col-md-3"></div>
-            <div class="col-md-6">
-                <div class="form-group form-group-search">
-                    <?= Html::hiddenInput('page', '1'); ?>
-                    <?= Html::textInput("keyword", $keyword, ['class' => 'form-control form-search', 'placeholder' => Yii::t('DirectoryModule.base', 'search for members')]); ?>
-                    <?= Html::submitButton(Yii::t('DirectoryModule.base', 'Search'), ['class' => 'btn btn-default btn-sm form-button-search']); ?>
-                </div>
+            <div class="form-group form-group-search">
+                <?= Html::hiddenInput('page', '1'); ?>
+                <?= Html::textInput("keyword", $keyword, ['class' => 'form-control form-search', 'placeholder' => Yii::t('DirectoryModule.base', 'Поиск')]); ?>
+                <?= Html::submitButton(Yii::t('DirectoryModule.base', '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="23" viewBox="0 0 24 23" fill="none"><path d="M22.7615 19.2878L17.387 13.7355C17.0016 13.3371 16.5199 13.1777 16.3112 13.3813C16.1018 13.5835 15.6198 13.4244 15.2344 13.0261L15.1575 12.9464C17.563 9.78271 17.3295 5.25302 14.4409 2.36445C11.2884 -0.788149 6.1768 -0.788149 3.0246 2.36445C-0.127993 5.51704 -0.127993 10.6279 3.0246 13.7805C5.96238 16.7186 10.5991 16.9154 13.7693 14.3778L13.803 14.413C14.1877 14.8113 14.331 15.298 14.1209 15.5006C13.9118 15.7032 14.0541 16.1902 14.4399 16.5878L19.8115 22.1423C20.1969 22.5396 20.8319 22.5514 21.2307 22.1656L22.7382 20.707C23.1354 20.3222 23.1462 19.6876 22.7615 19.2878ZM12.667 12.0063C10.4975 14.1762 6.96708 14.1762 4.79792 12.0071C2.62844 9.83758 2.62877 6.30679 4.79792 4.1377C6.96708 1.96822 10.4975 1.96855 12.6662 4.13803C14.8361 6.30719 14.8361 9.83725 12.667 12.0063Z" fill="white"/></svg>'), ['class' => 'btn btn-default btn-sm form-button-search']); ?>
             </div>
-            <div class="col-md-3"></div>
-        </div>
         <?= Html::endForm(); ?>
 
         <?php if (count($users) == 0): ?>
@@ -49,6 +43,7 @@ use humhub\modules\user\widgets\Image;
                 <div class="media">
                     <?= Image::widget([
                         'user' => $user,
+                        'width' => 48,
                         'htmlOptions' => ['class' => 'media-avatar'],
                         'linkOptions' => ['data-contentcontainer-id' => $user->contentcontainer_id]
                     ]); ?>
--- views/directory/directory/spaces.php
+++ views/directory/directory/spaces.php
@@ -15,24 +15,18 @@ use humhub\widgets\LinkPager;
 use yii\helpers\Url;
 
 ?>
-<div class="panel panel-default">
+<div class="panel panel-default section-spaces">
 
     <div class="panel-heading">
-        <?= Yii::t('DirectoryModule.base', '<strong>Space</strong> directory'); ?>
+        <?= Yii::t('DirectoryModule.base', '<strong>Список</strong> групп'); ?>
     </div>
 
     <div class="panel-body">
         <?= Html::beginForm(Url::to(['/directory/directory/spaces']), 'get', ['class' => 'form-search']); ?>
-        <div class="row">
-            <div class="col-md-3"></div>
-            <div class="col-md-6">
-                <div class="form-group form-group-search">
-                    <?= Html::textInput('keyword', $keyword, ['class' => 'form-control form-search', 'placeholder' => Yii::t('DirectoryModule.base', 'search for spaces')]); ?>
-                    <?= Html::submitButton(Yii::t('DirectoryModule.base', 'Search'), ['class' => 'btn btn-default btn-sm form-button-search']); ?>
-                </div>
+            <div class="form-group form-group-search">
+                <?= Html::textInput('keyword', $keyword, ['class' => 'form-control form-search', 'placeholder' => Yii::t('DirectoryModule.base', 'Поиск')]); ?>
+                <?= Html::submitButton(Yii::t('DirectoryModule.base', '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="23" viewBox="0 0 24 23" fill="none"><path d="M22.7615 19.2878L17.387 13.7355C17.0016 13.3371 16.5199 13.1777 16.3112 13.3813C16.1018 13.5835 15.6198 13.4244 15.2344 13.0261L15.1575 12.9464C17.563 9.78271 17.3295 5.25302 14.4409 2.36445C11.2884 -0.788149 6.1768 -0.788149 3.0246 2.36445C-0.127993 5.51704 -0.127993 10.6279 3.0246 13.7805C5.96238 16.7186 10.5991 16.9154 13.7693 14.3778L13.803 14.413C14.1877 14.8113 14.331 15.298 14.1209 15.5006C13.9118 15.7032 14.0541 16.1902 14.4399 16.5878L19.8115 22.1423C20.1969 22.5396 20.8319 22.5514 21.2307 22.1656L22.7382 20.707C23.1354 20.3222 23.1462 19.6876 22.7615 19.2878ZM12.667 12.0063C10.4975 14.1762 6.96708 14.1762 4.79792 12.0071C2.62844 9.83758 2.62877 6.30679 4.79792 4.1377C6.96708 1.96822 10.4975 1.96855 12.6662 4.13803C14.8361 6.30719 14.8361 9.83725 12.667 12.0063Z" fill="white"/></svg>'), ['class' => 'btn btn-default btn-sm form-button-search']); ?>
             </div>
-            <div class="col-md-3"></div>
-        </div>
         <?= Html::endForm(); ?>
 
         <?php if (count($spaces) == 0): ?>
@@ -40,45 +34,29 @@ use yii\helpers\Url;
         <?php endif; ?>
     </div>
 
-    <hr>
     <ul class="media-list">
         <?php foreach ($spaces as $space) : ?>
             <li>
                 <div class="media">
-                    <div class="pull-right">
-                        <?=
-                        MembershipButton::widget([
-                            'space' => $space
-                            ]);
-                        ?>
-                    </div>
-
                     <?= Image::widget([
-                        'space' => $space, 'width' => 50,
+                        'space' => $space,
+                        'width' => 48,
                         'htmlOptions' => [
                             'class' => 'media-object',
                             'data-contentcontainer-id' => $space->contentcontainer_id
                         ],
-                        'linkOptions' => ['class' => 'pull-left'],
+                        'linkOptions' => ['class' => 'media-avatar'],
                         'link' => true,
                     ]); ?>
-
-                    <?php if ($space->isMember()): ?>
-                        <i class="fa fa-user space-member-sign tt" data-toggle="tooltip" data-placement="top" title=""
-                           data-original-title="<?= Yii::t('DirectoryModule.base', 'You are a member of this space'); ?>"></i>
-                    <?php endif; ?>
 
                     <div class="media-body">
                         <h4 class="media-heading">
                             <?= Html::containerLink($space); ?>
-                            <?php if ($space->isArchived()) : ?>
-                                <span
-                                    class="label label-warning"><?= Yii::t('ContentModule.base', 'Archived'); ?></span>
-                            <?php endif; ?>
                         </h4>
+                    </div>
 
-                        <h5><?= Html::encode(Helpers::truncateText($space->description, 100)); ?></h5>
-                        <?= SpaceTagList::widget(['space' => $space]); ?>
+                    <div class="media-actions">
+                        <?= MembershipButton::widget(['space' => $space]); ?>
                     </div>
                 </div>
             </li>
