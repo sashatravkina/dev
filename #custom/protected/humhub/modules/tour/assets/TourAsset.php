17672ca53a5c2c25b735ca4a1536388a43979939
Alexandra Travkina <hello@fixel.ru>
2021-06-29 03:18:29 +0700

    Upload

---
+++ #custom/protected/humhub/modules/tour/assets/TourAsset.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * @link https://www.humhub.org/
+ * @copyright Copyright (c) 2015 HumHub GmbH & Co. KG
+ * @license https://www.humhub.com/licences
+ */
+
+namespace humhub\modules\tour\assets;
+
+use Yii;
+use yii\helpers\Url;
+use yii\web\AssetBundle;
+use humhub\modules\ui\view\components\View;
+
+/**
+ * Stream related assets.
+ *
+ * @since 1.2
+ * @author buddha
+ */
+class TourAsset extends AssetBundle
+{
+
+    /**
+     * @inheritdoc
+     */
+    public $sourcePath = '@tour/resources';
+
+    /**
+     * @inheritdoc
+     */
+    public $publishOptions = ['forceCopy' => false];
+
+    /**
+     * @inheritdoc
+     */
+    public $js = [
+        'js/bootstrap-tourist.min.js',
+        'js/humhub.tour.js'
+    ];
+
+    public $css = [
+        'css/bootstrap-tourist.min.css'
+    ];
+
+    /**
+     * @param View $view
+     * @return AssetBundle
+     */
+    public static function register($view)
+    {
+        $view->registerJsConfig('tour', [
+            'dashboardUrl' => Url::to(['/dashboard/dashboard']),
+            'completedUrl' => Url::to(['/tour/tour/tour-completed']),
+            'template' => '
+                <div class="popover tour" role="tooltip">
+                    <div class="arrow"></div>
+                    <h3 class="popover-title"></h3>
+                    <div class="popover-content"></div>
+                    <div class="popover-navigation">
+                        <div class="btn-group">
+                            <button class="btn btn-sm btn-default btn-prev" data-role="prev">'.Yii::t('custom', 'Назад').'</button>
+                            <button class="btn btn-sm btn-default btn-next" data-role="next">'.Yii::t('custom', 'Вперед').'</button>
+                        </div>
+                        <button class="btn btn-sm btn-default btn-end" data-role="end">'.Yii::t('TourModule.base', 'End guide').'</button>
+                    </div>
+                </div>'
+        ]);
+        return parent::register($view);
+    }
+}
+
