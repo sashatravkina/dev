e83f088febb0f5e982918a8db507f81f7d3f147d
Alexandra Travkina <hello@fixel.ru>
2021-06-29 14:10:31 +0700

    Upload

--- js/template.js
+++ js/template.js
@@ -1,49 +1,69 @@
-$(document).ready(function() {
-    $(".menu-icon-open").click(function(){
+$(document).ready(function () {
+    $(".menu-icon-open").click(function () {
         $(".mobile-navigation").addClass("expanded");
     });
-    $(".mobile-navigation .mobile-icon").click(function(){
+    $(".mobile-navigation .mobile-icon").click(function () {
         $(".mobile-navigation").removeClass("expanded");
     });
-    $(".mobile-navigation .mobile-close").click(function(){
+    $(".mobile-navigation .mobile-close").click(function () {
         $(".mobile-navigation").removeClass("expanded");
     });
-    $(".mobile-navigation .mobile-close").click(function(){
+    $(".mobile-navigation .mobile-close").click(function () {
         $(".account-menu").removeClass("expanded");
     });
 });
 
-$(document).ready(function() {
-    $(".account-menu-open").click(function(){
+$(document).ready(function () {
+    $(".account-menu-open").click(function () {
         $(".account-menu").addClass("expanded");
     });
 });
 
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 
-    var openSidebar = function(){
+    var openSidebar = function () {
         $('.account-menu').addClass('expanded');
         $('.toggle-menu').addClass('expanded');
         $('#toggle-menu').addClass('toggle-close');
     }
-    var closeSidebar = function(){
+    var closeSidebar = function () {
         $('.account-menu').removeClass('expanded');
         $('.toggle-menu').removeClass('expanded');
         $('#toggle-menu').removeClass('toggle-close');
     }
 
-    $('.toggle-menu').click( function(event) {
+    $('.toggle-menu').click(function (event) {
         event.stopPropagation();
-        if( $(this).is('.expanded') ) {
+        if ($(this).is('.expanded')) {
             closeSidebar();
         } else {
             openSidebar();
         }
     });
 
-    $(document).click( function(event){
-        if ( !$(event.target).closest('.account-menu').length ) {
-            closeSidebar();   
+    $(document).click(function (event) {
+        if (!$(event.target).closest('.account-menu').length) {
+            closeSidebar();
         }
     });
 });
+$('.sidebar .topbar-frame .topbar').scroll(function () {
+    if ($(this).scrollTop() > 0) {
+        $('.account-menu').removeClass('expanded');
+        $('.toggle-menu').removeClass('expanded');
+        $('#toggle-menu').removeClass('toggle-close');
+    }
+});
+
+function setDocHeight() {
+    document.documentElement.style.setProperty('--vh', `${window.innerHeight/100}px`);
+};
+
+window.addEventListener('resize', function () {
+    setDocHeight();
+});
+window.addEventListener('orientationchange', function () {
+    setDocHeight();
+});
+
+setDocHeight();
