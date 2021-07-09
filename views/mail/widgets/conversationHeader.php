ae60d7f706ddfa5d47772fcdeb3508ebedfefe65
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2021-07-09 17:45:09 -0400

    Fix: Unknown variable $link error

--- views/mail/widgets/conversationHeader.php
+++ views/mail/widgets/conversationHeader.php
@@ -1,23 +1,24 @@
 <?php
-    use humhub\libs\Html;
-    use humhub\modules\mail\widgets\ConversationSettingsMenu;
-    use humhub\modules\mail\widgets\ParticipantUserList;
-    use humhub\modules\user\widgets\Image;
-    use humhub\widgets\ModalButton;
-    use humhub\modules\mail\helpers\Url;
 
-    /* @var $message \humhub\modules\mail\models\Message */
+use humhub\libs\Html;
+use humhub\modules\mail\widgets\ConversationSettingsMenu;
+use humhub\modules\mail\widgets\ParticipantUserList;
+use humhub\modules\user\widgets\Image;
+use humhub\widgets\ModalButton;
+use humhub\modules\mail\helpers\Url;
 
-    // Max items (including show more button) to display, should be > 2
-    $maxUserImageEntries = 3;
+/* @var $message \humhub\modules\mail\models\Message */
 
-    $users = $message->users;
-    $userCount = count($users);
+// Max items (including show more button) to display, should be > 2
+$maxUserImageEntries = 3;
 
-    // We only display the show more button if we have more than one overlapping user
-    $maxUserImages = ($userCount === $maxUserImageEntries) ? $maxUserImageEntries : $maxUserImageEntries - 1;
+$users = $message->users;
+$userCount = count($users);
 
-    $userList = '';
+// We only display the show more button if we have more than one overlapping user
+$maxUserImages = ($userCount === $maxUserImageEntries) ? $maxUserImageEntries : $maxUserImageEntries - 1;
+
+$userList = '';
 ?>
 
 <div class="conversation-head-avatar">
@@ -34,7 +35,8 @@
 </div>
 
 <div class="conversation-head-info">
-    <?= Yii::t('MailModule.base', '{name}', ['name' => $link]) ?>
+    <?php $link = Html::beginTag('strong') . Html::containerLink($message->originator, ['style' => ['color' => $this->theme->variable('info')]]) . Html::endTag('strong'); ?>	
+        <?= Yii::t('MailModule.base', 'created by {name}', ['name' => $link]) ?>
     <?= ParticipantUserList::widget(['message' => $message, 'options' => ['class' => '']]) ?>
 </div>
 
